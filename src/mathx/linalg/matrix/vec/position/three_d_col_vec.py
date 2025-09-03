d4b604db716e9468bf888e348cdfc84f3c164e55
Mohammad Rahmani <mohammad.rahmani.xyz@gmail.com>
2025-09-03 22:44:20 +0200

    state removed top robotix

--- src/mathx/linalg/matrix/vec/position/three_d_col_vec.py
+++
@@ -1,8 +0,0 @@
-from typing import Union, Tuple
-import numpy as np
-from mathx.linalg.matrix.vec.col_vec import ColVec
-
-
-class ThreeDColVec(ColVec):
-    def __init__(self, raw_vec:Union[np.ndarray[float,float,float], Tuple[float, float, float]]):
-        super().__init__(raw_vec)
