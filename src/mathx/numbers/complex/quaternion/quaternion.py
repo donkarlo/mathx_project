ca07dc33bfda2dd77a046ef4684bcdf0474bee38
Mohammad Rahmani <mohammad.rahmani.xyz@gmail.com>
2025-10-24 21:01:56 +0200

    quaternion and other are positionless

---
+++ src/mathx/numbers/complex/quaternion/quaternion.py
@@ -0,0 +1,26 @@
+from mathx.linalg.vec.vec import Vec
+from physix.dimension.unit.vec import Vec as UnitedVec
+
+class Quaternion(Vec):
+    """i² = j² = k² = ijk = −1
+    ij = k, jk = i, ki = j
+    ji = −k, kj = −i, ik = −j"""
+    __slots__ = ("_qw", "_qx", "_qy", "_qz")
+    def __init__(self, qw:float, qx:float, qy:float, qz:float):
+        self._qw = qw
+        self._qx = qx
+        self._qy = qy
+        self._qz = qz
+        super().__init__([self._qw,self._qx,self._qy,self._qz])
+
+    def normalize(self):
+        pass
+
+    def conjugate(self):
+        pass
+
+    def multiply(self, other:"Quaternion"):
+        pass
+
+    def from_axis_angle(self, axis:float, theta:float):
+        pass
